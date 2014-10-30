e2a205dfe27e35451e575f64468561fe611c317a
Robin Boutros <niuage@gmail.com>
2014-10-30 19:43:03 -0400

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,26 @@
+module ApplicationHelper
+  def images
+    [
+      {
+        url: "https://lh3.googleusercontent.com/-tCZDcR-QG4g/U3YM6Yfq_5I/AAAAAAAAPjY/H3zSTfoRS3k/w1878-h1344-no/IMG_0059%2Bcopy.jpg",
+        title: "First real oil",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+      },
+      {
+        url: "https://lh6.googleusercontent.com/-u2u4TwswUXI/UWrftAqYwoI/AAAAAAAAPjY/SU865aj8SlA/w1944-h1344-no/IMG_0017.JPG",
+        title: "First real oil",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+      },
+      {
+        url: "https://lh5.googleusercontent.com/-lQiDpbGXnYg/UWrfQtw6PxI/AAAAAAAAPjY/TjzFdZpjQKM/w1958-h1344-no/new%2Biphone%2Bpix%2B003.JPG",
+        title: "First real oil",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+      },
+      {
+        url: "https://lh4.googleusercontent.com/-3WIJAUWE1Tg/TnJjOK8Z7GI/AAAAAAAAPjY/c1c44pRWmMw/w1736-h1344-no/Golden%2Begg%2Bstone%2Bbright.jpg",
+        title: "First real oil",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+      }
+    ]
+  end
+end
