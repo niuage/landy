52c05c018c480bcfb7e09794da51a8a50af97677
Robin Boutros <niuage@gmail.com>
2014-10-30 20:03:44 -0400

    design update, starting thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,11 @@ module ApplicationHelper
         url: "https://lh4.googleusercontent.com/-3WIJAUWE1Tg/TnJjOK8Z7GI/AAAAAAAAPjY/c1c44pRWmMw/w1736-h1344-no/Golden%2Begg%2Bstone%2Bbright.jpg",
         title: "First real oil",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+      },
+      {
+        url: "https://lh6.googleusercontent.com/-UqKw7xk8QP4/TnJjPpuMdfI/AAAAAAAAPjY/8rG9sRBavEE/w1034-h1430-no/IMG_0002.JPG",
+        title: "First real oil",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
       }
     ]
   end
