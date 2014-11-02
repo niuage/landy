d6f8534079ba213b030f128b9345c3f1a83f1e13
Robin Boutros <niuage@gmail.com>
2014-11-02 13:53:35 -0500

    white bg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,27 +4,37 @@ module ApplicationHelper
       {
         url: "https://lh3.googleusercontent.com/-tCZDcR-QG4g/U3YM6Yfq_5I/AAAAAAAAPjY/H3zSTfoRS3k/w1878-h1344-no/IMG_0059%2Bcopy.jpg",
         title: "First real oil",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel.",
+        category: "Watercolor",
+        date: "08-10-2014"
       },
       {
         url: "https://lh6.googleusercontent.com/-u2u4TwswUXI/UWrftAqYwoI/AAAAAAAAPjY/SU865aj8SlA/w1944-h1344-no/IMG_0017.JPG",
         title: "First real oil",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel.",
+        category: "Portrait",
+        date: "08-10-2014"
       },
       {
         url: "https://lh5.googleusercontent.com/-lQiDpbGXnYg/UWrfQtw6PxI/AAAAAAAAPjY/TjzFdZpjQKM/w1958-h1344-no/new%2Biphone%2Bpix%2B003.JPG",
         title: "First real oil",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel.",
+        category: "Oils",
+        date: "08-10-2014"
       },
       {
         url: "https://lh4.googleusercontent.com/-3WIJAUWE1Tg/TnJjOK8Z7GI/AAAAAAAAPjY/c1c44pRWmMw/w1736-h1344-no/Golden%2Begg%2Bstone%2Bbright.jpg",
         title: "First real oil",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel.",
+        category: "Acrylics",
+        date: "08-10-2014"
       },
       {
         url: "https://lh6.googleusercontent.com/-UqKw7xk8QP4/TnJjPpuMdfI/AAAAAAAAPjY/8rG9sRBavEE/w1034-h1430-no/IMG_0002.JPG",
         title: "First real oil",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu elit iaculis, fringilla orci vel.",
+        category: "landscape",
+        date: "08-10-2014"
       }
     ]
   end
